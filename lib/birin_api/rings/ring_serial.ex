0025964b046cbe5bc657567f46e023f516a0843f
sashman <sashman90@gmail.com>
2019-03-28 22:42:39 +0000

    Ring serial number generation

--- lib/birin_api/rings/ring_serial.ex
+++ lib/birin_api/rings/ring_serial.ex
@@ -16,22 +16,22 @@ defmodule BirinApi.Rings.RingSerial do
   
   """
   
-  def create_ring_series_list(ring_series_list, user_id) when is_list(ring_series_list) do
-    ring_series_list
-    |> Enum.map(fn ring_series -> 
-      :ok = create_ring_numbers_from_series(ring_series, user_id)
+#   def create_ring_series_list(ring_series_list, user_id) when is_list(ring_series_list) do
+#     ring_series_list
+#     |> Enum.map(fn ring_series -> 
+#       :ok = create_ring_numbers_from_series(ring_series, user_id)
 
-      Rings.create_ring_series(ring_series)
-    end)
+#       Rings.create_ring_series(ring_series)
+#     end)
 
-  end
+#   end
 
-  def create_ring_numbers_from_series(%{length: series_length, serial_start: serial_start, serial_end: serial_end} = ring_series, _) when is_map(ring_series) do
+  def ring_number_stream(series_length, serial_start, serial_end) do
 
     {prefix, start_num} = serial_parts(serial_start, series_length)
     {^prefix, end_num} = serial_parts(serial_end, series_length)
 
-    serial_number_stream(prefix, start_num, end_num)
+    serial_number_stream(prefix, String.to_integer(start_num), String.to_integer(end_num))
   end
 
   def serial_number_stream(prefix, start_num, end_num) do
--- test/birin_api/rings/ring_serial_test.exs
+++ test/birin_api/rings/ring_serial_test.exs
@@ -8,11 +8,20 @@ defmodule BirinApi.Rings.RingSerialTest do
     end
 
     @tag :me
-    test "serial_number_stream/2 returns a stream of serials numbers given a start and end numbers and a prefix" do
+    test "serial_number_stream/3 returns a stream of serials numbers given a start and end numbers and a prefix" do
         serial_numbers = RingSerial.serial_number_stream("ABC", 5, 9) |> Enum.to_list()
         assert serial_numbers |> length == 5
         assert serial_numbers == [
             "ABC5", "ABC6", "ABC7", "ABC8", "ABC9"
+        ]
+    end
+
+    @tag :me
+    test "ring_number_stream/3 returns a stream of serials numbers given a start and end serials and a length" do
+        serial_numbers = RingSerial.ring_number_stream(5, "ABC01", "ABC05") |> Enum.to_list()
+        assert serial_numbers |> length == 5
+        assert serial_numbers == [
+            "ABC01", "ABC02", "ABC03", "ABC04", "ABC05"
         ]
     end
 
