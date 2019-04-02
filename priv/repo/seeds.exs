7975b46e610398ef98fb0bb47605e2e54e99dcbb
sashman <sashman90@gmail.com>
2019-04-02 20:39:38 +0100

    Adding user seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+BirinApi.Repo.insert!(%BirinApi.Accounts.User{
+  auth_id: "abc1",
+  email: "joe@smith.com",
+  name: "Joe Smith",
+  license_number: "ABC1"
+})
+
+BirinApi.Repo.insert!(%BirinApi.Accounts.User{
+  auth_id: "abc2",
+  email: "jane@smith.com",
+  name: "Jane Smith",
+  license_number: "ABC2"
+})
+
+BirinApi.Repo.insert!(%BirinApi.Accounts.User{
+  auth_id: "abc3",
+  email: "bob@jones.com",
+  name: "Bob Jones",
+  license_number: "ABC3"
+})
