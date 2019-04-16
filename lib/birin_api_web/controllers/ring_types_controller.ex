7ccbe56f753744d7ca40f32def64558eac598afc
sashman <sashman90@gmail.com>
2019-04-16 21:43:37 +0100

    Adding ring types controller and endpoint

---
+++ lib/birin_api_web/controllers/ring_types_controller.ex
@@ -0,0 +1,14 @@
+defmodule BirinApiWeb.RingTypesController do
+  use BirinApiWeb, :controller
+
+  alias BirinApi.Rings
+
+  action_fallback(BirinApiWeb.FallbackController)
+
+  def index(conn, _params) do
+    ring_types = Rings.list_ring_series_counts_by_type()
+
+    conn
+    |> json(%{data: ring_types})
+  end
+end
--- lib/birin_api_web/router.ex
+++ lib/birin_api_web/router.ex
@@ -2,13 +2,14 @@ defmodule BirinApiWeb.Router do
   use BirinApiWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", BirinApiWeb do
-    pipe_through :api
-    resources "/users", UserController, except: [:new, :edit]
-    resources "/ring_numbers", RingNumberController, except: [:new, :edit]
-    resources "/ring_series", RingSeriesController, except: [:new, :edit]
+    pipe_through(:api)
+    resources("/users", UserController, except: [:new, :edit])
+    resources("/ring_numbers", RingNumberController, except: [:new, :edit])
+    resources("/ring_series", RingSeriesController, except: [:new, :edit])
+    get("/ring_types", RingTypesController, :index)
   end
 end
