d6e8da283172967922a82d7c6b13c3048439301f
sashman <sashman90@gmail.com>
2019-06-09 12:24:18 +0100

    Importing csv file async to stop the http request killing the import

--- lib/birin_api_web/controllers/ring_series_controller.ex
+++ lib/birin_api_web/controllers/ring_series_controller.ex
@@ -1,5 +1,6 @@
 defmodule BirinApiWeb.RingSeriesController do
   use BirinApiWeb, :controller
+  require Logger
 
   alias BirinApi.{
     Import,
@@ -7,7 +8,7 @@ defmodule BirinApiWeb.RingSeriesController do
     Rings.RingSeries
   }
 
-  action_fallback BirinApiWeb.FallbackController
+  action_fallback(BirinApiWeb.FallbackController)
 
   def index(conn, %{"user_id" => user_id}) do
     ring_series = Rings.list_ring_series_by_user_id(user_id)
@@ -52,17 +53,25 @@ defmodule BirinApiWeb.RingSeriesController do
 
   def import(conn, %{"ring_series_file" => ring_series_file = %Plug.Upload{}}) do
     extension = Path.extname(ring_series_file.filename)
-    amount_created = import_file(ring_series_file.path, extension)
+    series_stream = file_stream(ring_series_file.path, extension)
+
+    Task.async(fn ->
+      amount_created = import_stream(series_stream)
+
+      Logger.info("Imported #{amount_created} ring records")
+    end)
 
     conn
-    |> json(%{"created_count" => amount_created})
+    |> json(%{"importing" => %{source: extension, count: series_stream |> Enum.count()}})
   end
 
-  defp import_file(filepath, ".csv") do
-    {:ok, amount_created} =
-      filepath
-      |> Import.RingsSeries.from_csv_file()
-      |> Rings.create_ring_numbers_from_series(nil)
+  defp file_stream(filepath, ".csv") do
+    filepath
+    |> Import.RingsSeries.from_csv_file()
+  end
+
+  defp import_stream(series_stream) do
+    {:ok, amount_created} = Rings.create_ring_numbers_from_series(series_stream, nil)
 
     amount_created
   end
