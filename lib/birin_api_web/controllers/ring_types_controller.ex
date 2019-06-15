1b164fff99b710c80ee51777b8ece5f445f3c670
sashman <sashman90@gmail.com>
2019-06-15 19:57:27 +0100

    Endpoint for ring types for a user

--- lib/birin_api_web/controllers/ring_types_controller.ex
+++ lib/birin_api_web/controllers/ring_types_controller.ex
@@ -5,15 +5,19 @@ defmodule BirinApiWeb.RingTypesController do
 
   action_fallback(BirinApiWeb.FallbackController)
 
+  def index(conn, %{"user_id" => user_id}) do
+    ring_types = Rings.list_ring_series_counts_by_type_by_user_id(user_id) |> list_to_map()
+
+    overall_total = Enum.reduce(ring_types, 0, fn {_, total}, acc -> total + acc end)
+
+    conn
+    |> json(%{data: ring_types, total: overall_total})
+  end
+
   def index(conn, _params) do
     ring_types = Rings.list_ring_series_counts_by_type()
 
-    allocated_ring_types =
-      Rings.list_ring_series_allocated_counts_by_type()
-      |> Enum.map(fn %{type: type, total: total} ->
-        {type, total}
-      end)
-      |> Map.new()
+    allocated_ring_types = Rings.list_ring_series_allocated_counts_by_type() |> list_to_map()
 
     ring_types =
       ring_types
@@ -30,5 +34,13 @@ defmodule BirinApiWeb.RingTypesController do
 
     conn
     |> json(%{data: ring_types})
+  end
+
+  defp list_to_map(list) do
+    list
+    |> Enum.map(fn %{type: type, total: total} ->
+      {type, total}
+    end)
+    |> Map.new()
   end
 end
