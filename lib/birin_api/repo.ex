df09354a18481f5bf7f73210a8fe2d4c1a782315
sashman <sashman90@gmail.com>
2019-07-03 23:57:25 +0100

    Adding db url override

--- lib/birin_api/repo.ex
+++ lib/birin_api/repo.ex
@@ -2,4 +2,8 @@ defmodule BirinApi.Repo do
   use Ecto.Repo,
     otp_app: :birin_api,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
